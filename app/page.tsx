bf24cdba885e43db31cedfbcee2c7443e12e4ccf
masfeb01 <febri01@gmail.com>
2024-10-14 10:49:52 +0700

    disable spinner

--- app/page.tsx
+++ app/page.tsx
@@ -73,12 +73,12 @@ export default function Home() {
   return (
     <div className="container-fluid bg-white p-0">
       {/* <!-- Spinner Start --> */}
-      <div id="spinner"
+      {/* <div id="spinner"
         className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
         <div className="spinner-border text-primary" style={{ width: "3rem", height: "3rem" }} role="status">
           <span className="sr-only">Loading...</span>
         </div>
-      </div>
+      </div> */}
       {/* <!-- Spinner End --> */}
 
       <div className="container-fluid position-relative p-0">
